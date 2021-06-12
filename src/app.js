4d1acde7ffbf2805bc375ba3f3229ceeb25d2b21
DhanushaPerera07 <buddhika.dhanusha@gmail.com>
2021-06-12 14:08:57 +0530

    Config .env file and app.js file.

--- src/app.js
+++ src/app.js
@@ -1,1 +1,18 @@
+const Koa = require('koa');
 
+const app = new Koa();
+require('dotenv').config({path: 'resources/.env'});
+
+/* testing purposes. */
+app.use(ctx => {
+    console.log('koa works!');
+    ctx.response.body = 'Hello Koa!';
+});
+
+app.listen(process.env.PORT_NO, (err) => {
+    if (err) {
+        console.error(`Something went wrong when starting the server.`, err);
+    } else {
+        console.log(`Server started successfully at port no: ${process.env.PORT_NO}`);
+    }
+});
